88b385af6474d082722ab00850b0849ee5635972
Suzuki158 <2201100399sb@cyber-u.ac.jp>
2025-02-06 07:29:02 +0900

    add hello page

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
+//load libraries
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//load routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var helloRouter = require('./routes/hello')
 
+//generate express
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//load functions
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//app.use for routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/hello', helloRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,4 +45,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+//set app to express
 module.exports = app;
