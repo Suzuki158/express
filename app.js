79eeb687b6c5afee8b7f548828a23bf40cea0c36
Suzuki158 <2201100399sb@cyber-u.ac.jp>
2025-02-06 09:01:52 +0900

    add rest api service

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var logger = require('morgan');
 //load routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var helloRouter = require('./routes/hello')
+var helloRouter = require('./routes/hello');
+var notesRouter = require('./routes/notes');
 
 //generate express
 var app = express();
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
+app.use('/notes', notesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
