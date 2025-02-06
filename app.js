2e608623d46004c6b17d1d31ce6613ffa1891e80
Suzuki158 <2201100399sb@cyber-u.ac.jp>
2025-02-06 11:52:25 +0900

    add api codes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
-var catRouter = require('./routes/cat');
+var picsRouter = require('./routes/pictures');
 
 //generate express
 var app = express();
@@ -31,7 +31,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
 app.use('/notes', notesRouter);
-app.use('/cat', catRouter);
+app.use('/pictures', picsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/pictures.js
+++ routes/pictures.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const request = require('request');
 router.get('/', async (req, res) => {
-request('https://api.thecatapi.com/v1/images/search', function (error, response,
+request('https://picsum.photos/200/300?grayscale', function (error, response,
 body) {
 if (!error && response.statusCode == 200) {
 const data = JSON.parse(body);
