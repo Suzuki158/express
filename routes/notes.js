c7a6270229a5f7e7072f322ae0dab5ca8f2a4e92
Suzuki158 <2201100399sb@cyber-u.ac.jp>
2026-02-05 11:05:28 +0900

    Add Page

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://2201100399sb:afeui56Fja@cluster0.jf57x.mongodb.net/retryWrites=true&w=majority&appName=Cluster0";
+const uri = "xxx";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
